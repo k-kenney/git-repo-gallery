3e35e7036eb54986a2ded6716face56a1f2ab545
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-04-28 07:25:07 +0200

    pulled repo information to display the most recent repos in my Git repository

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // for profile infor to appear
 const overview = document.querySelector(".overview");
 const username = "k-kenney";
+const repoList = document.querySelector(".repo-list");
 
 const gitUserInfo = async function () {
   const userInfo = await fetch (`https://api.github.com/users/${username}`);
@@ -25,4 +26,20 @@ const displayGitUserInfo = function (info) {
     </div>
   `;
   overview.append(div);
+  getRepos();
+};
+
+const getRepos = async function () {
+  const fetchRepo = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const repoData = await fetchRepo.json();
+  displayRepos(repoData);
+};
+
+const displayRepos = function (repos) {
+  for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(repoItem);
+  }
 };
